9809cfce058a542082556138904b42824409d662
metterschling <metterschling@users.berlios.de>
2010-10-07 09:55:21 +0200

    Minor main module cleanup

--- main.cpp
+++ main.cpp
@@ -26,20 +26,11 @@
 #include <QApplication>
 #include "myform.h"
 
-
 int main(int argc, char* argv[])
 {
   printf("Start application->>>>");
   QApplication app(argc, argv);
-
-  //QApplication::addLibraryPath(QApplication::applicationDirPath() + "/plugins");
-  //QString Path = QApplication::applicationDirPath() + "/dll";
-  //QByteArray byteArray = Path.toAscii();
-  //const char *str      = byteArray.constData();
-  //printf("Start application->>>> %s ",str);
-
   MyForm* form = new MyForm;
   form->show();
   return app.exec();
 }
-
